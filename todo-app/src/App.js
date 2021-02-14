a42fb0e562154fa7b87c58d0b6d8ffdbaf102fa0
sidd-oo <sahoosiddharth3@gmail.com>
2021-02-14 14:16:48 +0530

    todos list gets updated with input button submission

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [todos, setTodos] = useState(["Take dog for a walk", "Get a job", "Love yourself often"]);
+  const [input,setInput] = useState("");
+
+  const addTodo = (event)=>{
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  }
   return (
     <div className="App">
       <h1>TODO List</h1>
+      <form>
+
+      <input type="text" value = {input} onChange = {event=>{setInput(event.target.value)}} placeholder="Task..."/>
+      <button type = "submit" onClick = {addTodo}>Add To Todo</button>
+
+      </form>
+
+      <ul>
+        {todos.map(todo=>{
+          return <li>{todo}</li>;
+        })}
+      </ul>
     </div>
   );
 }
